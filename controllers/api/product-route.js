53037e05a031282194a28533d90aa241cebbf866
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-29 20:59:08 -0500

    dev'd product routes and product seeds

--- controllers/api/product-route.js
+++ controllers/api/product-route.js
@@ -31,15 +31,57 @@ router.get("/:id", async (req, res) => {
     });
     !productID
       ? res.status(404).json({ message: "ERROR Product ID not found!" })
-      : res
-          .status(200)
-          .json({
-            product: productID,
-            message: `Product ${productID.product_name} found!`,
-          });
+      : res.status(200).json({
+          product: productID,
+          message: `Product ${productID.product_name} found!`,
+        });
   } catch (err) {
     res.status(500).json({ message: "ERROR Product ID not found!" });
   }
 });
 
+// put/update existing product
+router.put("/:id", async (req, res) => {
+  try {
+    const [numOfUpdatedRows, product] = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+      returning: true,
+    });
+    if (numOfUpdatedRows === 0) {
+      return res
+        .status(404)
+        .json({ message: "ERROR; Product and/or ID not found!" });
+    }
+    const updatedProduct = await Product.findByPk(req.params.id); // query the updated product separately
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .json({ message: "ERROR; Updated product not found!" });
+    }
+    res.status(200).json({
+      product: updatedProduct,
+      message: `Product ${updatedProduct.product_name} Updated!`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+// post new product
+router.post("/", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res
+      .status(200)
+      .json({
+        product: product,
+        message: `Product ${product.product_name} Created!`,
+      });
+  } catch (err) {
+    res.status(400).json({ message: "ERROR creating new product!" });
+  }
+});
 module.exports = router;
