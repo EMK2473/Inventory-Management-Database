4af3cec31b3e1a7489d1c789e96478e85270f26f
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-29 19:37:30 -0500

    db seeded, and established sequelize connection

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const seedCategories = require("./category-seed");
+const seedProducts = require("./product-seed");
+const seedTags = require("./tag-seed");
+const sequelize = require("../config/connection");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n~~~~~DATABASE~SYNCED~~~~~\n");
+  await seedCategories();
+  console.log("\n~~~~~CATEGORIES~SEEDED~~~~~\n");
+  await seedProducts();
+  console.log("\n~~~~~PRODUCTS~SEEDED~~~~~\n");
+  await seedTags();
+  console.log("\n~~~~~TAGS~SEEDED~~~~~\n");
+  process.exit(0);
+};
+
+seedAll();
