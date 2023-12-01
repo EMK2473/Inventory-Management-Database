58baea981408d51c233cfb424c32483692d2b850
GodSlappedMe <narampark92@gmail.com>
2023-11-30 19:47:07 -0500

    main, signup, and login handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // set up a Sequelize connection to a MySQL database
-const Sequelize = require('sequelize');
-require('dotenv').config(); 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) 
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const Sequelize = require("sequelize");
+require("dotenv").config();
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        PORT: 3001,
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 module.exports = sequelize;
