8728b2468c5b7d3492c87bbe39c372afd4c44f5e
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-12-02 13:09:26 -0500

    dev'd home routes for login and signup

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -2,19 +2,39 @@ const router = require("express").Router();
 const { Product, Category, User, Order } = require("../models");
 const withAuth = require("../utils/withAuth");
 
-// get all categories and render main view
-router.get("/", async (req, res) => {
-    try {
-      // const categoryData = await Category.findAll({
-      //   include: [{ model: User, attributes: ["username"] }],
-      // });
-      // const categories = categoryData.map((category) => category.get({ plain: true }));
-      res.render("homepage", {
-        // categories,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// /login home route
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login", {
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// /signup home route
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup", {
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// // get all categories and render main view
+// router.get("/", async (req, res) => {
+//     try {
+//       // const categoryData = await Category.findAll({
+//       //   include: [{ model: User, attributes: ["username"] }],
+//       // });
+//       // const categories = categoryData.map((category) => category.get({ plain: true }));
+//       res.render("homepage", {
+//         // categories,
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 module.exports = router;
