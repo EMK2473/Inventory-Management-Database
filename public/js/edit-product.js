38a7af18dbeefd4d145ca451737e9cd7b6934a23
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-12-04 20:09:00 -0500

    dev'd front end values for inventory management

--- public/js/edit-product.js
+++ public/js/edit-product.js
@@ -1,13 +1,13 @@
-const ItemPile = require("itempile");
-const { or } = require("sequelize");
-let test = 100;
-ItemPile.maxStackSize = 9999;
-const stock = new ItemPile("product_name", 10);
-const adjustment = new ItemPile("product_name", -1);
-console.log(stock)
-console.log(adjustment)
-stock.mergePile(adjustment);
+// const ItemPile = require("itempile");
+// const { or } = require("sequelize");
+// let test = 100;
+// ItemPile.maxStackSize = 9999;
+// const stock = new ItemPile("product_name", 10);
+// const adjustment = new ItemPile("product_name", -1);
+// console.log(stock)
+// console.log(adjustment)
+// stock.mergePile(adjustment);
 
-console.log(stock);
+// console.log(stock);
 
 
